b61820318d5ca94fcc51d1189f6608a188702b7d
Rawand Mohammed <rawvsmaw22@gmail.com>
2022-05-20 15:16:09 +0200

    added canvas

--- src/App.js
+++ src/App.js
@@ -3,60 +3,90 @@ import './App.css';
 import AppsIcon from '@mui/icons-material/Apps';
 import Button from '@mui/material/Button';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
-
+import classNames from "classnames";
 import  Gallery  from './components/Gallery.jsx';
+import { StayPrimaryLandscape } from "@mui/icons-material";
 function App() {
   const videoRef = useRef(null);
   const photoRef = useRef(null);
-
-
+  
+  
   const [captured, setCaptured] = useState(false)
-
+  
   const getStream = () => {
-    navigator.mediaDevices.getUserMedia({
-      video: {width: 1920, height: 1080}
-    })
-    .then(stream => {
-      let video = videoRef.current;
-      video.srcObject = stream;
-      video.play();
-
-    })
-    .catch(err => {
-      console.error(err);
-    })
+    if(!captured){
+      navigator.mediaDevices.getUserMedia({
+        video: {width: 342, height: 291}
+      })
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+  
+      })
+      .catch(err => {
+        console.error(err);
+      })
+    }else {
+      console.log(captured);
+    }
+    
   }
 
+  
+
+  const takePhoto = () => {
+    const width = 342;
+    const height =  291;
+
+    let video = videoRef.current;
+    let photo = photoRef.current;
+    
+    photo.width = width;
+    photo.height = height;
+
+    let ctx = photo.getContext("2d");
+    ctx.drawImage(video, 0, 0, width,height);
+    setCaptured(true)
+
+    
+  }
+
+  console.log(videoRef);
   useEffect(() => {
-    getStream();
+    getStream()
   },[videoRef])
 
+
+
+  let customClass = classNames({'stream__Box status': !captured, 'stream__Box':captured })
   return (
     <div className="App">
       <div className='galleryIcon__Container'>
-      <AppsIcon  sx={{ fontSize:45 }} 
-       color='primary'/>
+      <AppsIcon  style={{ fontSize:45,color: 'white' }} 
+       />
       </div>
       
       <div className='stream__Container'>
-        <div className='stream__Box'>
-          <video ref={videoRef}>
 
-          </video>
-        </div>
         
+        {!captured && <div className='stream__Box'>
+          <video ref={videoRef}></video>
+        </div>}
+        
+        <div className={customClass} >
+        <canvas  ref={photoRef}></canvas>
+        </div>
       </div>
       <div className='button__Container'>
-      <Button variant="outlined" startIcon={<CameraAltIcon />} style={{
-        fontSize: 20,
+      <Button onClick={!captured ? takePhoto : getStream} variant="outlined" startIcon={<CameraAltIcon />} style={{
+        color: "white",
+        fontSize: "20px",
         
       }}>
-  Föreviga ett ögonblick
+  {!captured  ? "Föreviga ett ögonblick" : "Fånga ett nytt ögonblick" }
 </Button>
       </div>
-      
-
-
       
     </div>
   );
