c12d0e213f55b7664ebed42eb5c064efab7abd59
Rawand Mohammed <rawvsmaw22@gmail.com>
2022-05-28 13:00:06 +0200

    sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,7 +18,7 @@ self.addEventListener("fetch", (event) => {
        caches.match(event.request).then(resp => {
            return resp || fetch(event.request).then(async response =>  {
                const cache = await caches.open(cacheData);
-               cache.put(event.request, response.clone())
+               cache.put(event.request, response.clone(), {cache: "no-store"})
                return response;
            })
        })
