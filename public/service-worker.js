499e1cd13557475a34ca383f6bdb191727ff38db
Rawisha <rawvsmaw22@gmail.com>
2022-05-28 11:21:58 +0200

    fixed service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,36 +2,32 @@ const cacheData = "appV1";
 
 const dataToCache = [
     'manifest.json',
+    'static/js/main.95991a24.js',
     'static/js/bundle.js',
     'favicon.ico',
     '/gallery',
     '/']
-this.addEventListener("install", (event) => {
 
-    event.waitUntil(
-        caches.open(cacheData).then((cache) => {
-            cache.addAll(dataToCache)
-        })
+self.addEventListener("install", (event) => {
 
-    )
+   self.skipWating()
+})
+
+self.addEventListener("fetch", (event) => {
+   event.respondWith(
+       caches.match(event.request).then(resp => {
+           return resp || fetch(event.request).then(async response =>  {
+               const cache = await caches.open(cacheData);
+               cache.put(event.request, response.clone())
+               return response;
+           })
+       })
+       )
 
 })
 
-this.addEventListener("fetch", (event) => {
-    if (!navigator.onLine) {
-        event.respondWith(
-            caches.match(event.request)
-                .then((resp) => {
-                    if (resp) {
-                        return resp
-                    }
-                })
-        )
-    }
 
-})
-
-this.addEventListener('activate', (e) => {
+self.addEventListener('activate', (e) => {
     const cacheWhitelist = [];
     cacheWhitelist.push(CACHE_NAME);
 
